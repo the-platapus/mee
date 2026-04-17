bf52c1b388b2cbe2f6287a6f2dd402b073aef3e7
Abdullah Aamir <107461225+the-platapus@users.noreply.github.com>
2026-04-17 17:18:14 +0500

    major design changes to match

--- src/components/MoreInfo.tsx
+++ src/components/MoreInfo.tsx
@@ -16,18 +16,18 @@ export default function MoreInfo() {
 
       <div className="sub-caption glass flex gap-[3vh] p-[3.5%] flex-col sm:flex-row">
         <div className="quotes">
-          <h2 className="text-[29px] font-lexend font-normal leading-[47px] m-0 text-[#ec933f] pb-0">
+          <h2 className="text-[29px] font-lexend font-normal leading-[47px] m-0 text-[white] pb-0">
             &quot;Those who look for seashells will find seashells; those who open them will find pearls.&quot;
           </h2>
-          <h3 className="text-[15px] font-lexend font-light leading-[30px] m-0 text-[#ec933f] pt-0">
+          <h3 className="text-[15px] font-lexend font-light leading-[30px] m-0 text-[white] pt-0">
             -Al-Ghazali
           </h3>
         </div>
         <div className="aim">
-          <h2 className="text-[69px] text-left font-lexend font-large leading-[1.2] m-0 text-[#ec933f] mb-4">
+          <h2 className="text-[69px] text-left font-lexend font-large leading-[1.2] m-0 text-[white] mb-4">
             I aim
           </h2>
-          <h3 className="text-[15px] text-left font-lexend font-light leading-[27px] m-0 text-[#ec933f] pt-0">
+          <h3 className="text-[15px] text-left font-lexend font-light leading-[27px] m-0 text-[white] pt-0">
             to blur the lines between humans and tech. I believe with good skills
             and practice; we can make great use of technology and fill the voids
             in our daily life. My goal is to find those voids and fulfill them
